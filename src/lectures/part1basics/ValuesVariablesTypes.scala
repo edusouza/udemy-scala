385fee805db39c1faa48b63dbfb19e8be74c7217
Eduardo Olievira de Souza <souza.eduardo@gmail.com>
2019-10-26 10:24:59 -0300

    Adicionando arquivos no outro computador

---
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,13 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+
+  val x = 42
+  print(x)
+
+  val aString: String = "hello"
+
+  val aBoolean: Boolean = true
+
+  val aChar: Char = 'a'
+}
