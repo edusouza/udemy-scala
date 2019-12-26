18c420e950b66c2f3af8c33fae738318a31fcf6a
Eduardo Oliveira de Souza <eduardo@contabilizei.com.br>
2019-12-26 13:53:22 -0300

    Functional Programming

---
+++ src/lectures/part3fp/WhatsAFunction.scala
@@ -0,0 +1,11 @@
+package lectures.part3fp
+
+object WhatsAFunction extends App {
+
+  // DREAM: use functions as first class elements
+  // problem: OOP
+}
+
+trait Action {
+  def execute(element: Int): String = ???
+}
