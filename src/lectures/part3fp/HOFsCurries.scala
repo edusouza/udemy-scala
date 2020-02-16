a68203b33fc785c05172c3ff9c31f44017f10e5d
Eduardo Olievira de Souza <souza.eduardo@gmail.com>
2020-02-16 11:38:22 -0300

    Higher Order Functions and Curries

--- src/lectures/part3fp/HOFsCurries.scala
+++ src/lectures/part3fp/HOFsCurries.scala
@@ -63,4 +63,16 @@ object HOFsCurries extends App {
      3. compose(f,g) => x => f(g(x))
         andThen(f,g) => x => g(f(x))
    */
+
+  def toCurry(f: (Int, Int) => Int): (Int => Int => Int) =
+    x => y => f(x, y)
+
+  def fromCurry(f: (Int => Int => Int)): (Int, Int) => Int =
+    (x,y) => f(x)(y)
+
+  def compose(f: Int => Int, g: Int => Int): Int => Int =
+    x => f(g(x))
+
+  def andThen(f: Int => Int, g: Int => Int): Int => Int =
+    x => g(f(x))
 }
