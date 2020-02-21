0cc1643e0a58356a6e25db708f1fe946a47cb8ac
Eduardo Olievira de Souza <souza.eduardo@gmail.com>
2020-02-20 23:09:58 -0300

    Tuples and Maps

--- src/lectures/part3fp/TuplesAndMaps.scala
+++ src/lectures/part3fp/TuplesAndMaps.scala
@@ -12,8 +12,33 @@ object TuplesAndMaps extends App{
   // maps - keys -> values
   val aMap: Map[String, Int] = Map()
 
-  val phonebook = Map(("Jim", 555), "Daniel" -> 789)
+  val phonebook = Map(("Jim", 555), "Daniel" -> 789).withDefaultValue(-1)
 
   println(phonebook)
+
+  // map ops
+  println(phonebook.contains("Jim"))
+  println(phonebook("Daniel"))
+  println(phonebook("Mary"))
+
+  // add a pairing
+  val newPairing = "Mary" -> 678
+  val newPhonebook = phonebook + newPairing
+  println(newPhonebook)
+
+  // functionals on maps
+  // map, flatMap, filter
+  println(phonebook.map(pair => pair._1.toLowerCase -> pair._2))
+
+  // filterKeys
+  println(phonebook.filterKeys(x => x.startsWith("J")))
+  // mapvalues
+  println(phonebook.mapValues(number => "0245-" + number))
+
+  // conversions to other collections
+  println(phonebook.toList)
+  println(List(("Daniel", 555)).toMap)
+  val names = List("Bob", "James", "Angela", "Mary", "Jim", "Daniel")
+  println(names.groupBy(name => name.charAt(0)))
 
 }
