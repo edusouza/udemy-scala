4cd2be3dd6e7668800a15b126bd979e4b902bf2c
Eduardo Olievira de Souza <souza.eduardo@gmail.com>
2020-02-22 10:29:35 -0300

    Exercises Tuples And Maps

--- src/lectures/part3fp/TuplesAndMaps.scala
+++ src/lectures/part3fp/TuplesAndMaps.scala
@@ -41,4 +41,97 @@ object TuplesAndMaps extends App{
   val names = List("Bob", "James", "Angela", "Mary", "Jim", "Daniel")
   println(names.groupBy(name => name.charAt(0)))
 
+  // EXERCISES
+  /*
+      1. What would happen if I had two original entries "Jim"-> 555 and "JIM" -> 900
+      2. Overly simplified social network based on maps
+        Person = String
+        - add a person to network
+        - remove
+        - friend (mutual)
+        - unfriend
+
+        - number of friends of a person
+        - person with most friends
+        - how many peoploe have NO FRIENDS
+        - if there a social connection between two people (direct or not)
+   */
+
+  val anotherMap = Map("Jim" -> 555, "JIM" -> 900)
+  println(anotherMap.map(pair => pair._1.toLowerCase -> pair._2))
+
+  def add(network: Map[String, Set[String]], person: String): Map[String, Set[String]] =
+    network + (person -> Set())
+
+  def friend(network: Map[String, Set[String]], a: String, b: String): Map[String, Set[String]] = {
+    val friendsA = network(a)
+    val friendsB = network(b)
+
+    network + (a -> (friendsA + b)) + (b -> (friendsB + a))
+  }
+
+  def unfriend(network: Map[String, Set[String]], a: String, b: String): Map[String, Set[String]] = {
+    val friendsA = network(a)
+    val friendsB = network(b)
+
+    network + (a -> (friendsA - b)) + (b -> (friendsB - a))
+  }
+
+  def remove(network: Map[String, Set[String]], person: String): Map[String, Set[String]] = {
+    def removeAux(friends: Set[String], networkAcc: Map[String, Set[String]]): Map[String, Set[String]] = {
+      if (friends.isEmpty) networkAcc
+      else removeAux(friends.tail, unfriend(networkAcc, person, friends.head))
+    }
+
+    val unfriended = removeAux(network(person), network)
+    unfriended - person
+  }
+
+  val empty: Map[String, Set[String]] = Map()
+  val network = add(add(empty, "Bob"), "Mary")
+  println(network)
+  println(friend(network, "Bob", "Mary"))
+  println(unfriend(friend(network, "Bob", "Mary"), "Bob", "Mary"))
+  println(remove(friend(network, "Bob", "Mary"), "Bob"))
+
+  val people = add(add(add(empty, "Bob"), "Mary"), "Jim")
+  val jimBob = friend(people, "Jim", "Bob")
+  val testNet = friend(jimBob, "Mary", "Bob")
+  println(testNet)
+
+  def nFriends(network: Map[String, Set[String]], person: String): Int =
+    if (!network.contains(person)) 0
+    else network(person).size
+
+  println(nFriends(testNet, "Bob"))
+
+  def mostFriends(network: Map[String, Set[String]]): String =
+    network.maxBy(pair => pair._2.size)._1
+
+  println(mostFriends(testNet))
+
+  def nPeopleWithNoFriends(network: Map[String, Set[String]]): Int =
+    network.filter(pair => pair._2.isEmpty).size
+
+  println(nPeopleWithNoFriends(testNet))
+
+  def socialConnection(network: Map[String, Set[String]], a: String, b: String): Boolean = {
+    // Este é o algoritmo Breadth-First Search (Busca em largura)
+    def bfs(target: String, consideredPeople: Set[String], discoveredPeople: Set[String]): Boolean = {
+      if (discoveredPeople.isEmpty) false
+      else {
+        val person = discoveredPeople.head
+        if (person == target) true
+        else if (consideredPeople.contains(person)) bfs(target, consideredPeople, discoveredPeople.tail)
+        else bfs(target, consideredPeople + person, discoveredPeople.tail ++ network(person))
+      }
+    }
+
+    bfs(b, Set(), network(a) + a)
+
+  }
+
+  println(socialConnection(testNet, "Mary", "Jim"))
+  println(socialConnection(network, "Mary", "Bob"))
+
 }
